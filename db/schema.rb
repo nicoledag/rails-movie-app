499f63a0a74b3b24ae10f22ac1fa30cae70b8458
nicoledag <nnardella@gmail.com>
2020-09-19 09:39:25 -0400

    Add Genre model and add to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_131555) do
+ActiveRecord::Schema.define(version: 2020_09_19_133719) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.integer "movies_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,16 @@
     # User.create(username: 'Nicole', email: 'nicole@gmail.com', password_digest: 'Movies123%')
     # User.create(username: 'Brendan', email: 'brendan@gmail.com', password_digest: 'Showtimes456$')
     # User.create(username: 'Joann', email: 'joann@gmail.com', password_digest: 'Funtimes33%')
+
+
+    # Movie.create(title: 'Quantum of Solice', length: '1h 46m')
+    # Movie.create(title: 'The Sleep Over', length: '1h 43m')
+    # Movie.create(title: 'Top Gun', length: '1h 49m')
+    # Movie.create(title: 'Spider Man 3', length: '2h 19m')
+    # Movie.create(title: 'How To Train Your Dragon', length: '1h 37m')
+    # Movie.create(title: 'Shrek', length: '1h 30m')
+    # Movie.create(title: 'The Addams Family (2019)', length: '1h 26m')
+    # Movie.create(title: 'Captain America: The First Avenger', length: '2h 4m')
+    # Movie.create(title: 'The Holiday', length: '2h 15m')
+
+    
