cc0bffb2f9be752ae3b9a357b43ffea724bdf335
nicoledag <nnardella@gmail.com>
2020-09-20 07:24:02 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root 'sessions#home'
+get '/signup' => 'users#new'
+get '/login' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+
+resources :users
+resources :movies
+resources :orders
+
+
+
+
 end
+
+
+
+
+
