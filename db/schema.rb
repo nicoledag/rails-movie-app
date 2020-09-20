a15961d6388aa97cbfd46e7594fec73cb6911f33
nicoledag <nnardella@gmail.com>
2020-09-20 11:00:17 -0400

    Add images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_234623) do
+ActiveRecord::Schema.define(version: 2020_09_20_142547) do
 
   create_table "auditoria", force: :cascade do |t|
     t.integer "seats_available"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_09_19_234623) do
     t.string "length"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "orders", force: :cascade do |t|
