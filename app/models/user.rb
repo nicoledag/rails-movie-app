fe83544a959aeedb057626aef363d26622ac3bba
nicoledag <nnardella@gmail.com>
2020-09-20 21:25:26 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    
     has_many :orders
+
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
+
 end
