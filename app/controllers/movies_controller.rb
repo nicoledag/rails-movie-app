d115c26f3c3246682aee3b7353d6343d8a18dbcb
nicoledag <nnardella@gmail.com>
2020-09-20 20:46:59 -0400

    Add index, show and set_movie private method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+
+
+    before_action :set_movie, only: [:show]
+
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+        # binding.pry
+    end
+
+
+    def set_movie
+        @movie = Movie.find_by(id: params[:id])
+        if !@movie
+          redirect_to root_path
+        end
+      end
+    
 end
