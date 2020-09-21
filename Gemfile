3d324dd54bd26b72045fa626a61d487a4ca97734
nicoledag <nnardella@gmail.com>
2020-09-21 13:54:22 -0400

    Add jquery javascript

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.10'
 gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 
-
+gem 'jquery-rails'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,13 @@ class OrdersController < ApplicationController
     end
 
     def create
-        binding.pry
+        # binding.pry
     end
+
+    private
+
+    def orders_params
+        params.require(:order).permit(:total_seats_selected, :total_cost, :credit_card_number, :expiration_date, :showtime_id, :user_id)
+    end
+
 end
