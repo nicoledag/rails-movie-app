d14c882cf8cc824f41031c6a42ee95739874c29e
nicoledag <nnardella@gmail.com>
2020-09-20 21:22:33 -0400

    Add class methods to format date and time

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
 
     has_many :genres
     has_many :showtimes
+
+
+    
 end
--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -2,4 +2,12 @@ class Showtime < ApplicationRecord
 
     belongs_to :movie
     has_many :orders
+
+    def format_date
+        self.date.strftime("%m/%d/%Y")
+    end
+
+    def format_time
+        self.start_time.strftime("%l:%M %p")
+    end
 end
