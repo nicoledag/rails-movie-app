3b5b62bf28cee3e17efdb2b2feaac0139060eee3
nicoledag <nnardella@gmail.com>
2020-09-20 22:58:31 -0400

    Create show item page and start order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,11 @@
 class OrdersController < ApplicationController
 
     def new
+        # binding.pry
+        @order = Order.new
+    end
+
+    def create
+        binding.pry
     end
 end
--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -1,5 +1,18 @@
 class ShowtimesController < ApplicationController
+    before_action :set_movie, only: [:show]
+
+    def new
+      @order = Order.new
+    end
 
     def show 
+      @order = Order.new
     end
+
+    def set_movie
+        @showtime = Showtime.find_by(id: params[:id])
+        if !@showtime
+          redirect_to root_path
+        end
+      end
 end
