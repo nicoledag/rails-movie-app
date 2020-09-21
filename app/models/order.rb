25949f5413d5747cb61f28fef5db70655868670f
nicoledag <nnardella@gmail.com>
2020-09-20 21:30:12 -0400

    Add validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
 
     belongs_to :showtime
     belongs_to :user
+
+    validates :credit_card_number, :expiration_date, :total_seats_selected, :total_cost, presence: true
+    validates :credit_card_number, uniqueness: true
+
 end
