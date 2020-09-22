50ab9bdbdbf294b10249ce81a5e5587f489b8e89
nicoledag <nnardella@gmail.com>
2020-09-22 03:14:15 -0400

    Add validations on order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,32 +14,32 @@ class OrdersController < ApplicationController
           end
         end
         
-    def create
-      # binding.pry
-          @user = current_user
-          @showtime = Showtime.find_by_id(params[:order][:showtime_id]) 
-          @order = @user.orders.build(orders_params) 
+      def create
+        # binding.pry
+            @user = current_user
+            @showtime = Showtime.find_by_id(params[:order][:showtime_id]) 
+            @order = @user.orders.build(orders_params) 
+            
+            if @showtime.seats_available <= 0 || @showtime.seats_available < @order.total_seats_selected
+              render :new 
+
+            elsif @order.save
+    
+              @cost = @showtime.seat_cost
+              @total_seats = @order.total_seats_selected
+              @total_cost = @cost * @total_seats
+              @order.update(total_cost: @total_cost)
+
+              @total_seats_selected = @order.total_seats_selected
+              @seats_available = @showtime.seats_available
+              @new_seats_available =  @seats_available - @total_seats_selected
+              @showtime.update(seats_available: @new_seats_available  )
           
-          if @showtime.seats_available <= 0 || @showtime.seats_available < @order.total_seats_selected
-            render :new 
-
-          elsif @order.save
-  
-            @cost = @showtime.seat_cost
-            @total_seats = @order.total_seats_selected
-            @total_cost = @cost * @total_seats
-            @order.update(total_cost: @total_cost)
-
-            @total_seats_selected = @order.total_seats_selected
-            @seats_available = @showtime.seats_available
-            @new_seats_available =  @seats_available - @total_seats_selected
-            @showtime.update(seats_available: @new_seats_available  )
-        
-            redirect_to order_path(@order)
-          else
-            render :new #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
-            end
-      end
+              redirect_to order_path(@order)
+            else
+              render :new #lets us call field w/errors.  Keeps inputted data.  #renders users/new form.
+              end
+        end
 
 
 
